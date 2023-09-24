2f27a5f704c34aec8e7aae1367c8bd2fc8272269
Azex01 <tobegoodman@gmail.com>
2023-09-24 18:42:39 +0300

    after solving conflict

--- sorter.java
+++ sorter.java
@@ -1,11 +1,8 @@
 class Main {
     public static void main(String[] args) {
 
-
-
-
     }
-<<<<<<< HEAD
+
     public static void insertionSort(int[] arr) {
 
         int n = arr.length;
@@ -18,32 +15,29 @@ class Main {
                 j = j - 1;
             }
             arr[j + 1] = key;
-=======
-    public class Main {
-        public static void main(String[] args)
-        {
 
-        }
-        public static void selectionSort(int[] arr) {
-            int n = arr.length;
+        }}
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-            for (int i = 0; i < n - 1; i++) {
-                int minIndex = i;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
 
-                // Find the index of the minimum element in the remaining unsorted portion
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j] < arr[minIndex]) {
-                        minIndex = j;
-                    }
+            // Find the index of the minimum element in the remaining unsorted portion
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
-
-                // Swap the found minimum element with the first element
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
             }
->>>>>>> bde963d9a0364843a1bb9b43fb2f7e9355957394
 
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-    }
-}
+
+}}
+
+
+
+
